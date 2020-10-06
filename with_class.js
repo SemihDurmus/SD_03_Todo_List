2239024d7fb825779cbe79445f961450b48dc6ac
SemihDurmus <semihdurmus@yahoo.com>
2020-10-06 14:21:10 +0200

    Add: js file with class

---
+++ with_class.js
@@ -0,0 +1,97 @@
+const todoObjectList = [];
+
+class Todo_Class {
+    constructor(x){
+        this.ulElement =x;
+    } 
+
+    add() {
+        const todoInput = document.querySelector("#myInput").value;
+        if (todoInput == "") {
+            alert("You did not enter any item!") 
+        } else {
+            const todoObject = {
+                id : todoObjectList.length,
+                todoText : todoInput,
+                isDone : false,
+            }
+
+        todoObjectList.unshift(todoObject);
+        this.display();
+        document.querySelector("#myInput").value = '';
+
+        }
+    }
+
+    done_undone(x) {
+        const selectedTodoIndex = todoObjectList.findIndex((item)=> item.id == x);
+        console.log(todoObjectList[selectedTodoIndex].isDone);
+        todoObjectList[selectedTodoIndex].isDone == false ? todoObjectList[selectedTodoIndex].isDone = true : todoObjectList[selectedTodoIndex].isDone = false;
+        console.log(todoObjectList[selectedTodoIndex].isDone);
+
+        this.display();
+    }
+
+    deleteElement(x) {
+        const selectedDelIndex = todoObjectList.findIndex((item)=> item.id == x);
+
+        todoObjectList.splice(selectedDelIndex,1);
+
+        this.display();
+    }
+
+
+    display() {
+        this.ulElement.innerHTML = "";
+
+        todoObjectList.forEach((object_item) => {
+
+            const liElement = document.createElement("li");
+            const delBtn = document.createElement("i");
+
+            liElement.innerText = object_item.todoText;
+            liElement.setAttribute("data-id", object_item.id);
+
+            delBtn.setAttribute("data-id", object_item.id);
+            delBtn.classList.add("far");
+            delBtn.classList.add("fa-trash-alt");
+
+            liElement.appendChild(delBtn);
+            
+            delBtn.addEventListener("click", function(e) {
+                const deleteId = e.target.getAttribute("data-id");
+                myTodoList.deleteElement(deleteId);
+            })
+            
+            liElement.addEventListener("click", function(e) {
+                const selectedId = e.target.getAttribute("data-id");
+                myTodoList.done_undone(selectedId);
+            })
+
+            if (object_item.isDone) {
+                liElement.classList.add("checked");
+            }
+
+            this.ulElement.appendChild(liElement);
+        })
+
+    }
+} 
+
+
+
+////-----MAIN PROGRAM------------
+const listSection = document.querySelector("#myUL");
+
+myTodoList = new Todo_Class(listSection);
+
+
+document.querySelector(".addBtn").addEventListener("click", function() {
+    myTodoList.add()
+})
+
+document.querySelector("#myInput").addEventListener("keydown", function(e) {
+    if (e.keyCode == 13) {
+        myTodoList.add()
+    }
+})
